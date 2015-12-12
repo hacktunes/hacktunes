a0eab3c32208f808b1d4b17493f179dc99acb2cf
Max Goodman <c@chromakode.com>
2015-12-11 21:31:20 -0800

    Move static directory into site

--- site/components/Page.js
+++ site/components/Page.js
@@ -6,7 +6,7 @@ export default function Page({title, html, scriptHash, cssName}) {
       <head>
         <meta charSet="utf-8" />
         <title>{title}</title>
-        <link rel="icon" href={require('../../static/favicon.png')} sizes="32x32" />
+        <link rel="icon" href={require('../static/favicon.png')} sizes="32x32" />
         {cssName && <link rel="stylesheet" type="text/css" id="css" href={'/' + cssName} />}
       </head>
       <body>
